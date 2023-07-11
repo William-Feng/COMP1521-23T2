52e9a3e7969ea237b1ac7e171ed17aeb2b508aaf
William Feng <williamfeng2001@gmail.com>
2023-07-11 16:31:51 +1000

    Add files for week 7

---
+++ Week 7/q6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t six_middle_bits(uint32_t u) {
+    /*
+    ____ ____ ____ ____ ____ ____ ____ ____ 
+    ____ ____ ____ _XXX XXX_ ____ ____ ____
+    
+    0000 0000 0000 0XXX XXX0 0000 0000 0000
+    0000 0000 0000 0000 0000 0000 00XX XXXX
+    */
+
+    // Method 1:
+    // Mask: 0000 0000 0000 0000 0000 0000 0011 1111
+    uint32_t mask = 0x3F;
+    return (u & (mask << 13)) >> 13;
+    
+    // Method 2 (ideal):
+    uint32_t mask = 0x3F;
+    return (u >> 13) & mask;
+    
+    // Method 3 (hacky):
+    return (u << 13) >> 26;
+}
